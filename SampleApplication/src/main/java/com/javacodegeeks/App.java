de1ae3b4c815ce5bd5291a27aee7105173821835
sumanthupar <43923464+sumanthupar@users.noreply.github.com>
2020-05-01 21:39:48 +0530

    Update App.java

--- SampleApplication/src/main/java/com/javacodegeeks/App.java
+++ SampleApplication/src/main/java/com/javacodegeeks/App.java
@@ -6,14 +6,12 @@ package com.javacodegeeks;
  */
 public class App {
 	
-	public String reverseString(String src) {
-		return new StringBuilder(src).reverse().toString();
-	}
+	
 	
     public static void main(String[] args) {
-    	final String str = "Hello!";
+ 
     	
         App appObject = new App();
-        System.out.println("The reverse string of \"" + str + "\" is \"" + appObject.reverseString(str) + "\"");
+        System.out.println("Hello world");
     }
 }
